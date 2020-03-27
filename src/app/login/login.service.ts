abb36e38a24c48164e4713a4bed6aeb1221aaf47
KowsalyaPHP <kowsalya@innovatussytems.com>
2020-03-27 11:33:27 +0530

    27-3-2020 CLient forms

---
+++ src/app/login/login.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Rx';
+import { AppComponent } from '../app.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: Http) { }
+
+  public LoginUser(FormObj): Observable<any> {
+
+    const url_login = AppComponent.urlPath + 'signin';
+    const params = new URLSearchParams();   
+    params.set('userID', FormObj.username);
+    params.set('password', FormObj.password);
+   
+    return this.http.post(url_login, params)
+      .map(response => response.json()).map(data => {
+        if (data != '')
+          return data;
+        else
+          return '';
+      });
+  }
+}
